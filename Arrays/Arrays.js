0fce9b05ce9adacf651a9f310613a108e36aa166
BheemeshCirpe <bheemesh.cirpe@gmail.com>
2022-07-16 01:55:13 +0530

    Arrays commit

---
+++ Arrays/Arrays.js
@@ -0,0 +1,15 @@
+const fruits = ["Banana", "Orange", "Apple"];
+
+
+for (let i = 0; i < fruits.length; i++) {
+    
+    console.log(fruits[i])
+  }
+var len=fruits[fruits.length+1] = "Lemon";
+
+console.log("*****************")
+
+for (let i = 0; i < fruits.length; i++) {
+    
+    console.log(fruits[i])
+  }
