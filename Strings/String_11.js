7b08f03597bccf52039a5ce5546e927f51395e33
BheemeshCirpe <bheemesh.cirpe@gmail.com>
2022-07-16 14:02:02 +0530

    testttt

---
+++ Strings/String_11.js
@@ -0,0 +1,13 @@
+var a = "This is a Life";
+var b = "This is b Life";
+
+if (a.length==b.length) {
+
+    console.log("if conditions satified")
+    console.log("done in if")
+}
+
+else {
+    console.log("else conditions satified")
+    console.log("done in else")
+}
---
+++ Variables/Variables_2.js
@@ -0,0 +1,17 @@
+var a;
+var b = "";;
+var c = null;
+var d = 1;
+e = d / 0;
+e1 = d - "Santhosh";
+
+console.log("*************************")
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(e1)
+console.log("*************************")
+
+
