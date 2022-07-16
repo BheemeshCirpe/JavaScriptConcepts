ebf64bef29d02aaea4b54baeaf09178a695a9fe2
BheemeshCirpe <bheemesh.cirpe@gmail.com>
2022-07-16 13:02:59 +0530

    changes 16-jul

---
+++ Debug/Test.js
@@ -0,0 +1,14 @@
+console.log("This is first Display")
+
+var a = 10;
+var b = 20;
+console.log(a)
+console.log(b)
+
+console.log("This is Second Display")
+var c = a + b;
+console.log("This is Third Display")
+
+console.log(c)
+
+console.log("This is fourth Display") 
--- Strings/String_10.js
+++ Strings/String_10.js
@@ -10,3 +10,5 @@ let b = "10";
 let c = a + b;
 
 console.log(c)
+
+//test
